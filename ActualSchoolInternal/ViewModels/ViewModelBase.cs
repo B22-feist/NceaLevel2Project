4da7e227620c05acea03d324a021c44f8a8dfa61
Bruno Feist <b22.feist@student.stjohns-hamilton.school.nz>
2025-04-29 10:06:34 +1200

    First Commit. Setting up Github

---
+++ ActualSchoolInternal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ActualSchoolInternal.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ActualSchoolInternal/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ActualSchoolInternal.Views;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
